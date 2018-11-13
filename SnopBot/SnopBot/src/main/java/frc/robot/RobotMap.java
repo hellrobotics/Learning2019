81c0d1fdc948295c91e4d0430e4fdcad90620efd
Knut <fatsheep200@gmail.com>
2018-11-13 16:53:06 +0100

    Initial setup
    startet opp prosjektet

---
+++ SnopBot/SnopBot/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,32 @@
+package src.main.java.frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	
+    //PWM
+    
+	//public static final int PWMNUMBER = 0;
+	
+	//CAN
+    
+    //public static final int CANNUMBER = 0;
+	
+    //DIO
+    
+	//public static final int DIONUMBER = 0;
+	
+	
+	
+	
+	
+
+	// If you are using multiple modules, make sure to define both the port
+	// number and the module. For example you with a rangefinder:
+	// public static int rangefinderPort = 1;
+	// public static int rangefinderModule = 1;
+}
